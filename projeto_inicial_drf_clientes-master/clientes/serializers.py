e4ee7e3d8ca92e8549f1508d29438263495e7a47
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-10-18 22:03:12 -0300

    Validações no Serializer

--- projeto_inicial_drf_clientes-master/clientes/serializers.py
+++ projeto_inicial_drf_clientes-master/clientes/serializers.py
@@ -1,8 +1,20 @@
+from clientes.validator import celular_valido, cpf_valido
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validator import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"O CPF deve ter 11 dígitos"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"O Nome não pode ter números"})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"O RG deve entre 11 e 14 dígitos"})
+        
+        return data
