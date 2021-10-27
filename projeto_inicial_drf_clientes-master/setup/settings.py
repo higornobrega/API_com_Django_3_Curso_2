ddea24781bfe50bfd7eee37f7943d497c62e9b3e
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-10-26 22:09:58 -0300

    Deploy do projeto

--- projeto_inicial_drf_clientes-master/setup/settings.py
+++ projeto_inicial_drf_clientes-master/setup/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -131,3 +132,5 @@ REST_FRAMEWORK = {
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
+
+django_heroku.settings(locals())
